304ac4c35eb836faa17d8b71afd206b4004c29b4
Logan Brock <logan.g.brock@gmail.com>
2021-02-25 20:51:37 -0600

    tic tac toe update

---
+++ js/main.js
@@ -0,0 +1,8 @@
+// handle the inputs
+// Show making a move - either an x or o
+// Show 3 in a row - diagonal or linear
+// check win if 3 in a row - diagonal or linear
+// if x selected then next selection o
+
+let currentSelection = 'X';
+
